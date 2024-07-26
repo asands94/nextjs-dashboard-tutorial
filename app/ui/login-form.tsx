4372a0462f96dfce6b9d8a1b8df843a0e70c1fda
Ashley Sands <ashley.sands01@gmail.com>
2024-07-26 11:12:22 -0400

    add email and password credentials on login page

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -39,8 +39,10 @@ export default function LoginForm() {
                 placeholder='Enter your email address'
                 required
               />
+
               <AtSymbolIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
+            user@nextmail.com
           </div>
           <div className='mt-4'>
             <label
@@ -61,6 +63,7 @@ export default function LoginForm() {
               />
               <KeyIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
+            123456
           </div>
         </div>
         <Button className='mt-4 w-full' aria-disabled={isPending}>
